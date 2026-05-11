e8aee56bb61d75d1a6b618079855105cb79cef0a
Ruslan Karabalin <karabalin112@gmail.com>
2026-05-11 22:32:34 +0300

    feat: add counters

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let count = $state(0);
+
+	let inc = () => {
+		++count;
+	};
+
+	let dec = () => {
+		++count;
+	};
+</script>
+
+<div style="display: flex; margin: 1rem 0;">
+	<button onclick={dec}>-</button>
+	<h1 style="margin: 1rem;">{count}</h1>
+	<button onclick={inc}>+</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,25 @@
+<script>
+	import Counter from '$lib/components/Counter.svelte';
+</script>
+
 <h1>Hi There</h1>
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+<Counter />
